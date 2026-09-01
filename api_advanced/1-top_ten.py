de28177a03f0d96ac46423ea92c7c7fdaecb61e1
Ansong26 <f.ansong@alustudent.com>
2026-09-01 11:58:23 +0400

    created the required files

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of first 10 hot posts."""
+    base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:alche-api-advanced:v1.0 (by /u/shania178)"
+    }
+
+    response = requests.get(
+        base_url,
+        headers=headers,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    posts = data["data"]["children"]
+
+    for post in posts[:10]:
+        print(post["data"]["title"])
